e986de46260660eeee64a7339e340bf1c8470e0e
J. Alex Baecher <jbaecher@ufl.edu>
2022-07-18 11:54:14 -0400

    Pushing changes to stratification drivers (figure code)

--- scripts/01_curation/03_stratification_drivers.R
+++ scripts/01_curation/03_stratification_drivers.R
@@ -1,4 +1,4 @@
-## David Klinges
+## Ed Basham, J. Alex Baecher, David Klinges
 ## This script curates drivers data - i.e. splitting up the categories and joining into a new dataframe for plotting
 
 
@@ -14,11 +14,30 @@ library(scales)
 # the first row (column names) first time through....
 
 driver_names <- c("structure", "food", "morphology", "interactions", "climate", "shelter", "seasonality", "light", "diurnality", "age", "sex")
+
 drivers <- read_csv("data/stripped_data/curation/drivers.csv") %>%
   pivot_longer(-c(sites, study_ID),
     names_to = c("type", ".value"),
     names_sep = "_") %>%
-  filter(type %in% driver_names)
+  filter(type %in% driver_names) %>%
+  mutate(type = recode(type, 
+                       "structure" = "Habitat Structure", 
+                       "food" = "Food / Foraging", 
+                       "morphology" = "Morphology", 
+                       "interactions" = "Species Interactions", 
+                       "climate" = "Climate", 
+                       "shelter" = "Nesting / Roosting", 
+                       "seasonality" = "Seasonality", 
+                       "light" = "Light", 
+                       "diurnality" = "Diurnality",  
+                       "age" = "Age", 
+                       "sex" = "Sex")) %>%
+  mutate(type = factor(type, levels = c(
+    "Habitat Structure", "Food / Foraging", "Climate", "Morphology", "Species Interactions", "Seasonality", "Nesting / Roosting","Light", "Diurnality", "Age", "Sex")))
+
+drivers %>%
+  group_by(type) %>%
+  summarize()
 
 ### merge with correct list of studies that are used in the analysis 
 
@@ -66,17 +85,6 @@ ggplot(drivers_summary_long) +
   scale_alpha_discrete(" ", labels = c("Investigated", "Referenced"), range = rev(c(0.6, 1)), guide = guide_legend(reverse=T)) +
   facet_wrap(~ factor(taxa, levels = c("Bats", "Birds", "Small mammals", "Amphibians")), ncol = 1) + 
   guides(fill = "none") +
-  scale_x_discrete(labels=c("Age", 
-                            "Climate", 
-                            "Diurnality", 
-                            "Food", 
-                            "Species interactions",  
-                            "Light",  
-                            "Morphology",
-                            "Seasonality", 
-                            "Sex",   
-                            "Shelter",  
-                            "Habitat structure")) +
   theme(legend.position = "top", legend.title = element_blank(),
         strip.background = element_rect(colour="black",
                                         fill="white"),
@@ -89,3 +97,4 @@ ggplot(drivers_summary_long) +
         axis.text.y=element_text(colour="black", size = 11),
         legend.text=element_text(colour="black", size = 11, face = "bold"),
         plot.margin = margin(1,1,1,1, "cm"))
+ggsave("figures/stratification_drivers.jpeg", dpi = 600)
